9ad93ae455bcfb7021a91a59c4c8d6c7003671cb
addy024 <phulallwaraditya1999@gmail.com>
2023-02-06 17:51:18 +0000

    Question3: 14851920

---
+++ flows/etl_gcs_to_bq.py
@@ -0,0 +1,54 @@
+from pathlib import Path 
+import pandas as pd 
+from prefect import flow, task 
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+
+@task(retries=3)
+def extract_from_gcs(color: str, year: int, month: int) -> Path:
+    """download trip data from GCS"""
+    gcs_path = f"{color}/{color}_tripdata_{year}-{month:02}.parquet"
+    gcs_block = GcsBucket.load("zoomcamp-gcs")
+    gcs_block.get_directory(from_path=gcs_path, local_path="")
+    return Path(f"{color}_trip_data_{year}-{month:02}.parquet")
+
+@task()
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    df = pd.read_parquet(path)
+    return df
+
+@task()
+def write_bq(df: pd.DataFrame) -> None:
+    """write DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("zoomcamp-gcp-creds")
+    df.to_gbq(
+            destination_table="dezoomcamp.rides",
+            project_id="data-engineering-375013",
+            credentials=gcp_credentials_block.get_credentials_from_service_account(),
+            chunksize=500_000,
+            if_exists="append"
+            )
+
+
+@flow() 
+def etl_gcs_to_bq(color, year, month):
+    """main etl flow to load data into BQ"""
+    
+    path = extract_from_gcs(color, year, month)
+    df = transform(path)
+    write_bq(df)
+    return df 
+
+
+@flow()
+def rows_processed(months=[2, 3], year=2019, color="yellow"):
+    """main flow prints total bumber of rows processed"""
+    count = 0
+    for month in months:
+        df = etl_gcs_to_bq(color, year, month)
+        count += len(df)
+    print(f"Total no. of rows processed: {count}")
+
+if __name__ == "__main__":
+    rows_processed()
