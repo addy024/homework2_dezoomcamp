9a556e76cc64a3609da8739c34c92068f02f59a8
addy024 <phulallwaraditya1999@gmail.com>
2023-02-06 18:04:06 +0000

    question4

---
+++ flows/github.py
@@ -0,0 +1,25 @@
+from pathlib import Path 
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """The main ETL function"""
+    dataset_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2020-11.csv.gz"
+
+    df = fetch(dataset_url)
+    print(len(df))
+
+
+
+if __name__ == "__main__":
+    etl_web_to_gcs()
